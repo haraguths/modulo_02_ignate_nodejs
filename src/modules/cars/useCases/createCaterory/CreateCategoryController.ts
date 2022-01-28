2f1a3db05a1f4a0b30ef3c9f0f97be7e89429574
Yuri Haraguth <haraguths@gmail.com>
2022-01-28 07:40:02 -0300

    Teste

--- src/modules/cars/useCases/createCaterory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCaterory/CreateCategoryController.ts
@@ -7,7 +7,6 @@ class CreateCategoryController {
 
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
-    
     this.createCategoryUseCase.execute({ name, description });
 
     return response.status(201).send();
