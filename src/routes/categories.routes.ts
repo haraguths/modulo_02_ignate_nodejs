f7899b40a72376cf54a340ed1a43eba685b97899
Yuri Haraguth <haraguths@gmail.com>
2022-01-30 15:28:22 -0300

    Upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCaterory";
 import { listCategoryController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const categoriesRoutes = Router();
 
@@ -13,4 +18,13 @@ categoriesRoutes.get("/", (request, response) => {
   listCategoryController.handle(request, response);
 });
 
+categoriesRoutes.post(
+  "/imports",
+  upload.single("file"),
+  (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+  }
+);
 export { categoriesRoutes };
